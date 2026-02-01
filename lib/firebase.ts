7e5e21fccd6bc0488f4928a9b587999942d4e46f
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2026-01-31 22:01:31 -0330

    remove log

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,5 @@
-// lib/firebase.ts
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-// DEBUG: Log what we're getting
-console.log("ENV CHECK:", {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-});
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -17,8 +9,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
-console.log("Firebase Config:", firebaseConfig);
 
 const app =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
